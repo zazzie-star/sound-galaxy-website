e7cf89fa5bb3675092048d11b5a7fcb4b5b88c38
Zazzie Wharton <zazziewharton@gmail.com>
2026-08-03 13:19:18 +0100

    giving particle object basic properties

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let fft
 
 let Particle = function (position) {
-    
+    this.position = position //setting position given to particle when its created
+    this.speed = createVector(0, 1) //setting spped to 0 in x direction and 1 in y direction
+    this.color = [random(0, 255), random(0, 255), random(0, 255)] //setting color to a random RGB color
 }
 
 function setup() {
